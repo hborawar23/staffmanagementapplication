2213dc288b575d9a573c3819f5ef159bd009755a
Himanshi Borawar <Himanshi.Borawar@programmers.io>
2023-05-10 16:06:59 +0530

    add class level comment

--- src/main/java/com/SDS/staffmanagement/controller/HrController.java
+++ src/main/java/com/SDS/staffmanagement/controller/HrController.java
@@ -3,6 +3,10 @@ package com.SDS.staffmanagement.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+/*
+    Add class level comment
+ */
+
 @Controller
 @RequestMapping("/hr")
 public class HrController {
